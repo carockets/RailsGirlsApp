fe07380922d6168e725928311c4214012417c92a
Carola Boettcher <boettcher.carola@gmail.com>
2012-10-13 12:06:11 +0200

    first railsgirls app

---
+++ app/models/idea.rb
@@ -0,0 +1,9 @@
+class Idea < ActiveRecord::Base
+
+  mount_uploader :picture, PictureUploader
+
+  attr_accessible :description, :name, :picture
+
+  
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsgirls::Application.config.secret_token = '86f94661975515984b49d421886ebea4d2cbb13833060d5c492168556c48a9bec57b3f5f44cb2aff96ce5c6da062b8213f99addaad13a26baafa075b54be43ab'
